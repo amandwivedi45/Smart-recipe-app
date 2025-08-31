35e5753d5aa48f9957d3332a2306e2e00c25838b
Aman Dwivedi <145118324+amandwivedi45@users.noreply.github.com>
2025-08-31 12:36:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import requests
 from urllib.parse import unquote
@@ -80,5 +81,6 @@ def view_recipe(recipe_id):
     return "Recipe not found", 404
 
 # Run the app in debug mode if executed directly
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway gives PORT dynamically
+    app.run(host="0.0.0.0", port=port)
